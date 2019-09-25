b59c8fc4ea7038dbea330aeeb97fe930a542c7b0
Yuno <yuno99825@gmail.com>
2019-09-25 13:55:23 +1200

    Create warning alert if user does not select any text to preview

--- src/application/PreviewController.java
+++ src/application/PreviewController.java
@@ -1,5 +1,7 @@
 package application;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 
 import java.io.IOException;
@@ -13,11 +15,17 @@ public class PreviewController {
 
     public void go() throws IOException {
         String text = searchResultsArea.getSelectedText();
-        if (text.isEmpty()) {
-            text = searchResultsArea.getText();
+        if (!text.isEmpty() || searchResultsArea.getSelectedText().isEmpty()) {
+            String cmd = "echo \"" + text + "\" | festival --tts";
+            ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", cmd);
+            Process process = builder.start();
+        } else {
+            // Create warning alert
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("No text selected");
+            alert.setHeaderText("Please select some text to preview");
+            alert.setContentText("Select text to preview by highlighting it.");
+            alert.showAndWait();
         }
-        String cmd = "echo \"" + text + "\" | festival --tts";
-        ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", cmd);
-        Process process = builder.start();
     }
 }
