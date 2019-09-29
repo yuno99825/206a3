aa8cb4c2dd7486031f5af8df36d6764eaa89f7a7
Yuno <yuno99825@gmail.com>
2019-09-29 23:46:21 +1300

    Repositioned replay button on top of video player

--- src/application/VideoPlayerController.java
+++ src/application/VideoPlayerController.java
@@ -18,6 +18,8 @@ public class VideoPlayerController {
     @FXML
     private MediaView mediaView;
     private MediaPlayer player;
+    @FXML
+    private Button replayButton;
 
     public void setUpVideo(String creationName, Stage stage) {
         File videoURL = new File("./creations/" + creationName + "/creation.mp4");
@@ -26,6 +28,7 @@ public class VideoPlayerController {
         player.setOnReady(() -> {
             stage.sizeToScene();
         });
+        player.setOnEndOfMedia(() -> replayButton.setVisible(true));
         player.setAutoPlay(true);
         mediaView.setMediaPlayer(player);
         mediaView.fitWidthProperty().bind(stage.widthProperty());
@@ -44,7 +47,9 @@ public class VideoPlayerController {
 
     @FXML
     private void replayButtonClicked() {
+        replayButton.setVisible(false);
         player.seek(Duration.ZERO);
+        player.play();
     }
 
     public void stopVideo() {
