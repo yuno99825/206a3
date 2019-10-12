8d9a8aab380700d1226fe90c0f577c562ab4ec10
Yuno <yuno99825@gmail.com>
2019-10-12 15:59:08 +1300

    Added spacing and padding to ImageSelection screen and intialized images in controller

--- src/application/ImageSelectionController.java
+++ src/application/ImageSelectionController.java
@@ -1,12 +1,21 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
 
 public class ImageSelectionController {
+    @FXML
+    private TilePane imagesTilePane;
 
     @FXML
     private void initialize() {
-
+        int i = 1;
+        for (Node node: imagesTilePane.getChildren()) {
+            ImageView imageView = (ImageView) node;
+            imageView.setImage(new Image(".temp/images/" + i + ".jpg"));
+        }
     }
-
 }
